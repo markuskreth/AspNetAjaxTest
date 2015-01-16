40aace3cf1b87924764594e07019e21cdfdecc42
Markus Kreth <mk@ansat.de>
2015-01-16 13:47:58 +0100

    Bugfix: GetSqlInt32  ersetzt durch GetInt32, Abfrage nach Koordinaten

--- AspNetAjaxTest/AjaxService.svc.cs
+++ AspNetAjaxTest/AjaxService.svc.cs
@@ -1,75 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Runtime.Serialization;
+using System.Globalization;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
-using System.ServiceModel.Web;
-using System.Text;
 
 namespace AspNetAjaxTest
 {
-    [ServiceContract(Namespace = "AspNetAjaxTest")]
-    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-    public class AjaxService
-    {
-        private SqlConnection con = new SqlConnection("Data Source=VIRTUALBOX;Initial Catalog=Ast;Persist Security Info=True;User ID=Markus;Password=0773");
+   [ServiceContract(Namespace = "AspNetAjaxTest")]
+   [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+   public class AjaxService
+   {
 
-        [OperationContract]
-        public WebHst[] getHst(int minHstPs, int limit)
-        {
-            List<WebHst> result = new List<WebHst>();
-            using (con)
+      private SqlConnection con = new SqlConnection("Data Source=tfs2010\\sqlexpress;Initial Catalog=EWFAst;Persist Security Info=True;User ID=Ansat;Password=ansatuser");
+
+      [OperationContract]
+      public WebHst[] getHst(int minHstPs, int limit)
+      {
+         List<WebHst> result = new List<WebHst>();
+         using (con)
+         {
+            SqlCommand cmd = new SqlCommand("SELECT top " + limit + " [hstPs], [HstBez] FROM [Hst] WHERE hstPs>" + minHstPs + " order by hstPs", con);
+            try
             {
-                SqlCommand cmd = new SqlCommand("SELECT top " + limit + " [hstPs], [HstBez] FROM [Hst] WHERE hstPs>" + minHstPs + " order by hstPs", con);
-                try
-                {
-                    con.Open();
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    while (rdr.Read())
-                    {
-                        WebHst hst = new WebHst();
-                        hst.HstPs = rdr.GetInt32(0);
-                        hst.HstBez = rdr.GetString(1);
-                        result.Add(hst);
-                    }
-                }
-                catch (Exception)
-                {
-                }
+               con.Open();
+               SqlDataReader rdr = cmd.ExecuteReader();
+               while (rdr.Read())
+               {
+                  WebHst hst = new WebHst();
+                  hst.HstPs = rdr.GetInt32(0);
+                  hst.HstBez = rdr.GetString(1);
+                  result.Add(hst);
+               }
             }
-            return result.ToArray();
-        }
-
-        [OperationContract]
-        public string getHstByPs(int hstPs)
-        {
-            using (con)
+            catch (Exception)
             {
-                SqlCommand cmd = new SqlCommand("SELECT  [hstPs], [HstBez], [gemeindePs] FROM [Hst] WHERE hstPs=" + hstPs, con);
-                try
-                {
-                    con.Open();
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    if (rdr.Read())
-                    {
-                        return rdr.GetSqlInt32(0) + ": " + rdr.GetString(1) + " - Gemeinde=" + rdr.GetInt32(2);
-                    }
-                }
-                catch (Exception e)
-                {
-                    return e.Message;
-                }
             }
-            return hstPs + ": Eine Haltestelle";
-        }
-    }
+         }
+         return result.ToArray();
+      }
 
-    public class WebHst
-    {
-        public int HstPs { get; set; }
+      [OperationContract]
+      public string getHstByPs(int hstPs)
+      {
+         using (con)
+         {
+            SqlCommand cmd = new SqlCommand("SELECT  [hstPs], [HstBez], [HstBreite], [HstLaenge] FROM [Hst] WHERE hstPs=" + hstPs, con);
+            try
+            {
+               con.Open();
+               SqlDataReader rdr = cmd.ExecuteReader();
+               if (rdr.Read())
+               {
+                  string hstBez = rdr.GetString(1);
+                  double breite = rdr.GetDouble(2);
+                  double laenge = rdr.GetDouble(3);
+                  return rdr.GetInt32(0) + ": " + hstBez + " - Koord=[" + breite.ToString("0.0#######################", CultureInfo.InvariantCulture) + "," + laenge.ToString("0.0#######################", CultureInfo.InvariantCulture) + "]";
+               }
+            }
+            catch (Exception e)
+            {
+               return e.Message;
+            }
+         }
+         return hstPs + ": Eine Haltestelle";
+      }
+   }
 
-        public string HstBez { get; set; }
-    }
+   public class WebHst
+   {
+      public int HstPs { get; set; }
+
+      public string HstBez { get; set; }
+   }
 }
